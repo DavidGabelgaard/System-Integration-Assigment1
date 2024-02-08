ca2ad3df3aad2033a5cd99872e47b80a8b1ad3f8
DavidGabelgaard <davi493c@edu.easj.dk>
2024-02-08 15:23:03 +0100

    Finished api from Python

--- System Integration Python/System Integration Python/Users.py
+++ System Integration Python/System Integration Python/Users.py
@@ -2,15 +2,22 @@ from typing import List
 from dataclasses import dataclass, field
 import xml.etree.ElementTree as ET
 
-@dataclass
-class UserModel:
-    FirstName: str = None
-    LastName: str = None
-    Valid: bool = None
-    Address: str = None
-    Age: int = None
-    Education: List[str] = field(default_factory=list)
 
-@dataclass
-class UserListModel:
-    Users: List[UserModel] = field(default_factory=list)
+class UserModel(object):
+    def __init__(self, FirstName, LastName, Valid, Address, Age, Education):
+       self.FirstName = FirstName
+       self.LastName = LastName
+       self.Valid = Valid
+       self.Address = Address
+       self.Age = Age
+       self.Education = Education
+    def __str__(self) -> str:
+        return f"UserModel: FirstName={self.FirstName}, LastName={self.LastName}, Valid={self.Valid}, Address={self.Address}, Age={self.Age}, Education={self.Education}"
+
+
+class UserListModel(object):
+    def __init__(self, Users):
+        self.Users = Users
+    def __str__(self) -> str:
+        user_strings = [str(user) for user in self.Users]
+        return '\n'.join(user_strings)
--- System Integration Python/System Integration Python/hello.py
+++ System Integration Python/System Integration Python/hello.py
@@ -1,9 +1,25 @@
 from flask import Flask
-from System_Integration_Python import load_users_from_txt
+from System_Integration_Python import load_users_from_json, load_users_from_csv, load_users_from_txt, load_users_from_xml, load_users_from_yaml
 
 app = Flask(__name__)
 
 @app.route("/txt")
-def hello_wolrd():
-    return "Test"
+def get_users_as_string_from_txt():
+    return load_users_from_txt('../../DataFiles/User.txt').__str__()
+
+@app.route("/json")
+def get_users_as_string_from_json():
+    return load_users_from_json('../../DataFiles/User.json').__str__()
+
+@app.route("/csv")
+def get_users_as_string_from_csv():
+    return load_users_from_csv('../../DataFiles/User.csv').__str__()
+
+@app.route("/xml")
+def get_users_as_string_from_xml():
+    return load_users_from_xml('../../DataFiles/User.xml').__str__()
+
+@app.route("/yaml")
+def get_users_as_string_from_yaml():
+    return load_users_from_yaml('../../DataFiles/User.Yaml').__str__()
 
